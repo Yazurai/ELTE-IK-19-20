b88a5ba1bbe14a0ce389bdbca3461b93f4eeee01
Tran Bach Khoa <tbkhoa98@gmail.com>
2019-09-14 12:22:52 +0200

    Final version - Added hcd, bugfixes

--- main.c
+++ main.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "main.h"
 
-enum services {HELLO = 0, FTOC = 1, SIGN = 2, MAXIMUM = 3, FACTORIAL = 4, REMAIN = 5, MULTITABLE = 6, EXIT = 7};
+typedef enum services_ {HELLO = 0, FTOC = 1, SIGN = 2, MAXIMUM = 3, FACTORIAL = 4, REMAIN = 5, MULTITABLE = 6, HCD = 7, EXIT = 8} services;
 
-int main()
-{
+int main() {
     bool shouldExit = false;
-    while(!shouldExit)
-    {
-        printf("Hi! Choose from the possible services by typing their number!\n Hello(0), FtoC(1), sign(2),"
-               "maximum(3), factorial(4), remain(5), multiTable(6), Exit(7)\n");
-        enum services currService;
-        scanf(" %d", &currService);
-        switch(currService)
-        {
+    while(!shouldExit) {
+        printf("\nHi! Choose from the possible services by typing their number!\n Hello(0), FtoC(1), sign(2),"
+               "maximum(3), factorial(4), remain(5), multiTable(6), highestCommonDivisor(7), Exit(8)\n");
+        services currService;
+        scanf(" %u", &currService);
+        switch(currService) {
         case HELLO:
-            hello(); break;
+            hello();
+            break;
         case FTOC:
-            FtoC(); break;
+            FtoC();
+            break;
         case SIGN:
-            sign(); break;
+            sign();
+            break;
         case MAXIMUM:
-            maximum(); break;
+            maximum();
+            break;
         case FACTORIAL:
-            factorial(); break;
+            factorial();
+            break;
         case REMAIN:
-            remain(); break;
+            remain();
+            break;
         case MULTITABLE:
-            multiTable(); break;
+            multiTable();
+            break;
+        case HCD:
+            hcd();
+            break;
         case EXIT:
-            shouldExit = true; break;
+            shouldExit = true;
+            break;
         default:
             printf("Wrong input, try again!\n");
         }
@@ -39,72 +48,53 @@ int main()
 }
 
 //Reads in a float value from the console to the given float by the pointer
-bool readFloat(float *num)
-{
+bool readFloat(float *num, char *message) {
+    printf("%s", message);
     int check = scanf(" %f", num);
-    if(check != 0)
-    {
+    if(check != 0) {
         return true;
-    }
-    else
-    {
+    } else {
         printf("Rossz formatum!\n");
         return false;
     }
 }
 
 //Reads in an int (could merge with float method)
-bool readInt(int *num)
-{
+bool readInt(int *num, char *message) {
+    printf("%s", message);
     int check = scanf(" %d", num);
-    if(check != 0)
-    {
+    if(check != 0) {
         return true;
-    }
-    else
-    {
+    } else {
         printf("Rossz formatum!\n");
         return false;
     }
 }
 
-void hello()
-{
+void hello() {
     printf("[HELLO]\n");
     printf("Hello world\n");
 }
 
-void FtoC()
-{
-    printf("[FAHRENHEIT TO CELCIUS]\n");
-    printf("Please enter a temperature in Fahrenheit: ");
+void FtoC() {
     float farenheit, celsius;
-    if(readFloat(&farenheit))
-    {
+    printf("[FAHRENHEIT TO CELCIUS]\n");
+    if(readFloat(&farenheit, "Please enter a temperature in Fahrenheit: ")) {
         celsius = (farenheit - 32) * 5 / 9;
         printf("%fF = %fC!\n", farenheit, celsius);
     }
 }
 
-void sign()
-{
-    printf("[SIGN]\n");
-    printf("Please enter a number: ");
+void sign() {
     float num;
-    if(readFloat(&num))
-    {
-        if(num < 0)
-        {
+    printf("[SIGN]\n");
+    if(readFloat(&num, "Please enter a number: ")) {
+        if(num < 0) {
             printf("The sign of the number is -1.\n");
-        }
-        else
-        {
-            if(num > 0)
-            {
+        } else {
+            if(num > 0) {
                 printf("The sign of the number is 1.\n");
-            }
-            else
-            {
+            } else {
                 printf("The sign of the number is 0.\n");
             }
         }
@@ -112,79 +102,85 @@ void sign()
 }
 
 //Easily expandable to any number of elements
-void maximum()
-{
-    printf("[MAXIMUM]\n");
-    printf("Please enter the number of elements.")
+void maximum() {
     int count;
-    if(readInt(&count)) 
-    {
-		return;
-	}
+    printf("[MAXIMUM]\n");
+    if(!readInt(&count, "Please enter the number of elements: ")) {
+        return;
+    }
     float nums[count];
-    int numCount = 0;
-    for(int i = 0; i < count; i++)
-    {
+    for(int i = 0; i < count; i++) {
         printf("Please enter number (%d/%d): ", i, count);
-        if(!readFloat(nums + sizeof(float) * i))
-        {
-			return;
+        if(!readFloat(nums + sizeof(float) * i, "")) {
+            return;
         }
     }
     float biggest = *nums;
-    for(int i = 1; i < 3; i++)
-    {
-        if(biggest < *(nums + sizeof(float) * i))
-        {
+    for(int i = 1; i < count; i++) {
+        if(biggest < *(nums + sizeof(float) * i)) {
             biggest = *(nums + sizeof(float) * i);
         }
     }
-    printf("The biggest number of the %d is: %f\n", numCount, biggest);
+    printf("The biggest number of the %d is: %f\n", count, biggest);
 }
 
-void factorial()
-{
-    printf("[FACTORIAL]\n");
-    printf("Please enter a number: ");
+void factorial() {
     int n, sum = 0;
-    if(readInt(&n))
-    {
-        for(int i = 1; i < n + 1; i++)
-        {
+    printf("[FACTORIAL]\n");
+    if(readInt(&n, "Please enter a number: ")) {
+        for(int i = 1; i < n + 1; i++) {
             sum += i;
         }
         printf("The factorial of !%d is %d\n", n, sum);
     }
 }
 
-void remain()
-{
+void remain() {
     printf("[REMAINDER]\n");
     int dividend, divisor;
     printf("Please enter the dividend: ");
-    if(scanf(" %d", &dividend) == 0)
-    {
+    if(scanf(" %d", &dividend) == 0) {
         return;
     }
     printf("Please enter the divisor: ");
-    if(scanf(" %d", &divisor) == 0)
-    {
+    if(scanf(" %d", &divisor) == 0) {
         return;
     }
-    while(dividend > 0)
-    {
+    while(dividend > 0) {
         dividend -= divisor;
     }
     printf("The remainder is %d.\n", divisor + dividend);
 }
 
-void multiTable()
-{
+void hcd() {
+    int a, b;
+    if(!readInt(&a, "Please enter the first number!")){
+        return;
+    }
+    if(!readInt(&b, "Please enter the first number!")){
+        return;
+    }
+    if(a < 0 || b < 0) {
+        return;
+    }
+    if(a < b) {
+        int oldA = a;
+        a = b;
+        b = oldA;
+    }
+    int remainder = a % b;
+    while (remainder > 0) {
+        a = b;
+        b = remainder;
+        remainder = a % b;
+    }
+    printf("The highest common divisor is %d", b);
+}
+
+void multiTable() {
     printf("[MULTIPLICATION TABLE]\n");
-    for(int y = 1; y < 11; y++)
-    {
-        for(int x = 1; x < 11; x++)
-        {
+    for(int y = 1; y < 11; y++) {
+        for(int x = 1; x < 11; x++) {
             printf("%d * %d = %d, ", y, x, y * x);
         }
         printf("\n");
---
+++ main.h
@@ -0,0 +1,8 @@
+void hello();
+void FtoC();
+void sign();
+void maximum();
+void factorial();
+void remain();
+void hcd();
+void multiTable();
