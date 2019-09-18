7219d8cc01fd48098daa795cf4bfd7b8d3c8a84f
Tran Bach Khoa <saof2t@INF.ELTE.HU>
2019-09-18 12:41:30 +0200

    Started new years eve countdown

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <stdbool.h>
 
+#define V 24*60*60
+#define GAPYEAR [31,28,31,30,31,30,31,31,30,31,30,31]
+#define NOTGAPYEAR [31,29,31,30,31,30,31,31,30,31,30,31]
+
 using namespace std;
+
+typedef struct date{
+    int year;
+    int month;
+    int day;
+    int hours;
+    int minutes;
+    int seconds;
+} date;
 
 float readNumber(bool isNatural, string name) {
     float x;
@@ -84,6 +97,45 @@ int mod() {
     return remainder;
 }
 
+bool isSkipYear(int year){
+    return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+}
+
+void leftUntilYearEnd(){
+    date d = {0,0,0,0,0,0}, rem = {0,0,0,0,0,0};
+
+    cout << "Enter the time int Y M D H M S format." << endl;
+    cin >> d.year >> d.month >> d.day >> d.hours >> d.minutes >> d.seconds;
+
+    rem.seconds = 59 - d.seconds;
+    rem.minutes = 59 - d.minutes;
+    rem.hours = 23 - d.hours;
+    if(isSkipYear(d.year)){
+        rem.day = GAPYEAR[d.month] - d.day;
+    } else {
+        rem.day = NOTGAPYEAR[d.month] - d.day;
+    }
+    rem.month = 12 - d.month;
+
+
+}
+/*
+void dayLeft(){
+    int h, m, s;
+
+    cout << "Enter the time int H M S format." << endl;
+    cin >> h >> m >> s;
+
+    int remaining = V - h*60*60 - m*60 - s;
+    int rH = remaining / 60 / 60;
+    remaining %= (60*60);
+    int rM = remaining / 60;
+    remaining %= 60;
+    int rS = remaining;
+
+    cout << "Remaining: " << rH << ", " << rM << ", " << rS << endl;
+}
+*/
 int main() {
     int funcCode;
     bool shouldStop = false;
@@ -106,8 +158,11 @@ int main() {
         case 4:
             cout << mod() << endl;
             break;
+        case 5:
+            break;
         default:
             break;
         }
     }
 }
+
