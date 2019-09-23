22b92845a6fa1e0e695c6e685deefc743faa6ec2
Tran Bach Khoa <tbkhoa98@gmail.com>
2019-09-23 19:47:47 +0200

    Week 3 python directory restructure

---
+++ PY/Week 3/HW/HUFtoEUR.py
@@ -0,0 +1,11 @@
+n = 0.0
+inputSuccess = False
+while not inputSuccess:
+    inputSuccess = True
+    try:
+        n = int(input("Please enter n:"))
+    except ValueError:
+        inputSuccess = False
+        print('Please enter a valid number!')
+for n in range(1, n, 1):
+    print(str(n) + "HUF is " + str(n * 0.00299728261) + "EUR.")
