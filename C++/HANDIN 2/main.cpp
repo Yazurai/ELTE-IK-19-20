6636ba487551b46d9a4178f30ca892d594d5c710
Tran Bach Khoa <tbkhoa98@gmail.com>
2019-10-10 13:26:02 +0200

    finalised c++ handin 2

--- C++/HANDIN 2/main.cpp
+++ C++/HANDIN 2/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main(){
+int main() {
     //Getting input data
     int n;
     string name;
@@ -14,7 +14,7 @@ int main(){
 
     //Filling up the data structure
     vector <string> days;
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         string newLine;
         getline(cin, newLine);
         days.push_back(newLine);
@@ -22,23 +22,23 @@ int main(){
 
     //Calculating the results
     vector <int> foundDays;
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         bool found = false;
         stringstream ns(days[i]);
         string n;
-        while(getline(ns, n, ',')){
-            if(n.compare(name) == 0){
+        while(getline(ns, n, ',')) {
+            if(n.compare(name) == 0) {
                 found = true;
             }
         }
-        if(found){
+        if(found) {
             foundDays.push_back(i+1);
         }
     }
 
     //Printing the results
     cout << foundDays.size();
-    for(int i = 0; i < foundDays.size(); i++){
+    for(int i = 0; i < foundDays.size(); i++) {
         cout << " " << foundDays[i];
     }
 }
