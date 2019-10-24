7eb3ed5f222435cd85156bc5dc434170d6d62dc0
Tran Bach Khoa <saof2t@INF.ELTE.HU>
2019-10-24 13:39:20 +0200

    Semi finished haskell zh5

--- HS/ZHs/5. Interpreter/zh.hs
+++ HS/ZHs/5. Interpreter/zh.hs
@@ -24,9 +24,11 @@ every n xs = [xs!!y | y <- [0..(length xs - 1)], y `mod` n == 0]
 modifyPtr :: Integral a => (a -> a) -> ([a],[a]) -> ([a],[a])
 modifyPtr f (xs, y:ys) = (xs, (f y):ys)
 
+--This deletes all occurences of ']'
 getLoopBody :: String -> (String,String)
 getLoopBody xs = foldr (\x (acc,rmd) -> if x == ']' && rmd == "" then ("",acc) else (x:acc,rmd)) ("","") xs
 
+--This is the correct version, it might be possible to do with a fold
 getLoopBody' :: String -> (String,String)
 getLoopBody' [] = ("","")
 getLoopBody' (x:xs)
@@ -42,20 +44,29 @@ loopUntil check f n x
     where
         newElem = (iterate f x)!!n
 
+--Doesnt work for nested loops.
+--The issue seems to be with not being able to predetermine the loop length for the loopuntil function
+--Tried to do it with countlength but that still doesnt solve the issue 
 run :: (([Int],[Int]),(String,String)) -> (([Int],[Int]),(String,String))
 run (memory,('>':cs, ds)) = (ptrInc memory, (cs,ds))
 run (memory,('<':cs, ds)) = (ptrDec memory, (cs,ds))
 run (memory,('+':cs, ds)) = (modifyPtr (+1) memory, (cs,ds))
 run (memory,('-':cs, ds)) = (modifyPtr (+(-1)) memory, (cs,ds))
 run (memory,('.':cs, ds)) = (memory, (cs,ds++[chr(readPtr memory)] ))
-run (memory,('[':cs, ds)) = state
+run (memory,('[':cs, ds))
+    |readPtr memory == 0  = (memory,(rnd,ds))
+    |otherwise            = ((m1,m2),(rnd,out))
     where
-        (loopBody,rnd) = getLoopBody cs
-        state = loopUntil (\(m,_) -> readPtr m == 0) run 1 (memory,(loopBody, ds))
-run (memory,(']':cs, ds)) = state
+        (loopBody,rnd)    = getLoopBody cs
+        ((m1,m2),(c,out)) = loopUntil (\(m,_) -> readPtr m == 0) run (countLength loopBody) (memory,(cycle loopBody, ds))
 run (m, c)                = (m, c)
 
-interpret :: String -> String
+countLength :: String -> Int
+countLength [] = 0
+countLength (x:xs)
+    |x == '['  = 1
+    |otherwise = countLength xs + 1
+
 interpret cs = out
     where
-        (_,(_,out)) = last (iterate run (emptyData,(cs,"")))
+        (_,(_,out)) = last (take (length cs + 1) $ iterate run (emptyData,(cs,"")))
